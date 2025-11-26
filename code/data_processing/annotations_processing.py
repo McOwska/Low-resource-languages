d705cb0884e4dfa9e525057098871d31da4cdb63
mcowska <zuza.m.makowska@gmail.com>
2025-11-26 19:15:21 +0100

    scripts for creating cv-like dataset

---
+++ code/data_processing/annotations_processing.py
@@ -0,0 +1,101 @@
+import soundfile
+import os
+import csv
+from librosa import load
+import textgrid
+import re
+import string
+import unicodedata
+
+transcription_tiers_map = {
+    "tcc": r"^Asmjeeg$",
+    "201": r"^ref",
+    "IGS": r"Transcription"
+}
+
+DIR_PATH = '../../data/6_11_2025_tcc'
+OUT_PATH = '../../data/6_11_2025_tcc_annotations'
+
+def remove_in_brackets_annotations(text):
+    without = re.sub(r"\s*\([^)]*\)", "", text)
+    return without.strip()
+
+def remove_dialog_annotations(text):
+    without = re.sub(r"[0-9]+;-", "", text)
+    return without.strip()
+
+def remove_alternatives(text):
+    return text.split(' / ')[0]
+
+def remove_interpunction(text):
+    PUNCT = r"\.\,\!\?\"\-\_\:"
+    DIGITS = r"0-9"
+    return re.sub(f"[{PUNCT}{DIGITS}]", "", text)
+
+def remove_diacritics(text):
+    normalized = unicodedata.normalize("NFD", text)
+    without_marks = "".join(ch for ch in normalized if not unicodedata.combining(ch))
+    return unicodedata.normalize("NFC", without_marks)
+
+def process_text(text, f_remove_dialogs=True, f_remove_brackets=True, f_remove_alternatives=True, f_remove_interpunction=True, f_remove_diacretics=True):
+    if f_remove_dialogs:
+        text = remove_dialog_annotations(text)
+    if f_remove_brackets:
+        text = remove_in_brackets_annotations(text)
+    if f_remove_alternatives:
+        text = remove_alternatives(text)
+    if f_remove_interpunction:
+        text = remove_interpunction(text)
+    if f_remove_diacretics:
+        text = remove_diacritics(text)
+    
+    return text
+    
+
+def process(file_name, folder_path, out_f):
+    tg = textgrid.TextGrid.fromFile(os.path.join(folder_path, file_name + '.TextGrid')) 
+    tier_name_pattern = transcription_tiers_map[file_name[:3]]
+    
+    for tier in tg.tiers:
+        if not re.match(tier_name_pattern, tier.name):
+            continue
+
+        non_empty_intervals = [interval for interval in tier.intervals if interval.mark.strip()]
+        if not non_empty_intervals:
+            print('THERE ARENT ANY NON EMPTY INTERVALS :(', folder_path, file_name)
+            return 0
+        
+        rel_folder = os.path.relpath(folder_path, DIR_PATH)
+        out_f.write(f"{rel_folder}/{file_name}\n")
+        
+        for interval in non_empty_intervals:
+            text = interval.mark.strip()
+            processed = process_text(text)
+            if processed != text:
+                print('_original:', text)
+                print('processed:', processed)
+            out_f.write(processed + "\n")
+        
+        out_f.write("\n")
+        
+        return 1 
+    return 0
+            
+
+def main():
+    os.makedirs(OUT_PATH, exist_ok=True)
+    out_file_path = os.path.join(OUT_PATH, "annotations_processed_2.txt")
+
+    directories = [d for d in os.listdir(DIR_PATH) if os.path.isdir(os.path.join(DIR_PATH, d))]
+
+    with open(out_file_path, "w", encoding="utf-8") as out_f:
+        for dir in directories:
+            inner_path = os.path.join(DIR_PATH, dir)
+            files = {file.split('.')[0] for file in os.listdir(inner_path) if file.endswith('.TextGrid')}
+            
+            for file in files:
+                process(file, inner_path, out_f)
+
+
+if __name__ == "__main__":
+    main()
---
+++ code/data_processing/create_cv_ds.py
@@ -0,0 +1,150 @@
+import annotations_processing
+import os
+import textgrid
+import re
+import soundfile
+import csv
+import numpy as np
+
+DIR_PATH = '../../data/6_11_2025_tcc'
+OUT_PATH = '../../data/6_11_2025_tcc_cv'
+
+transcription_tiers_map = {
+    "tcc": r"^Asmjeeg$",
+    "201": r"^ref",
+    "IGS": r"Transcription"
+}
+
+eng_tiers_map = {
+    "tcc": r"^English$",
+    "201": r"^??",
+    "IGS": r"??"
+}
+
+sw_tiers_map = {
+    "tcc": r"^Swahili$",
+    "201": r"^??",
+    "IGS": r"??"
+}
+
+def load_audio_for_file(folder_path, file_name):
+    exts = [".wav", ".flac", ".mp3", ".ogg"]
+    audio_path = None
+    for ext in exts:
+        candidate = os.path.join(folder_path, file_name + ext)
+        if os.path.exists(candidate):
+            audio_path = candidate
+            break
+
+    if audio_path is None:
+        print("NO AUDIO FILE FOUND FOR:", folder_path, file_name)
+        return None, None
+
+    audio, sr = soundfile.read(audio_path)
+    if audio.ndim > 1:
+        audio = np.mean(audio, axis=1)
+
+    return audio, sr
+
+def _ensure_output_dir(output_path):
+    clips_path = os.path.join(output_path, 'clips')
+    os.makedirs(clips_path, exist_ok=True)
+
+def _ensure_tsv_header(tsv_path):
+    if not os.path.exists(tsv_path):
+        with open(tsv_path, "w", encoding="utf-8") as f:
+            w = csv.writer(f, delimiter="\t")
+            w.writerow(["path","sentence","eng", "sw"])
+
+def append_row(tsv_path, audio_path, sentence, eng, sw):
+    with open(tsv_path, "a", encoding="utf-8") as f:
+        w = csv.writer(f, delimiter="\t")
+        w.writerow([audio_path, sentence, eng, sw])
+
+def save_segment(output_dir, segment_id, audio_file, sr, asmjeeg_str, eng_str, sw_str):
+    audio_path = os.path.join(output_dir, 'clips', segment_id + '.mp3')
+    _ensure_output_dir(output_dir)
+    soundfile.write(audio_path, audio_file, sr)
+
+    tsv_path = os.path.join(output_dir, 'dataset.tsv')
+    _ensure_tsv_header(tsv_path)
+    audio_relative_path = os.path.join('clips', segment_id + '.mp3')
+    append_row(tsv_path, audio_relative_path, asmjeeg_str.strip(), eng_str.strip(), sw_str.strip())
+
+def find_translations(tiers, interval, file_type):
+    eng_name_pattern = eng_tiers_map[file_type]
+    sw_name_pattern = sw_tiers_map[file_type]
+    eng, sw = '', ''
+    for tier in tiers:
+        if re.match(eng_name_pattern, tier.name):
+            for eng_interval in tier.intervals:
+                if eng_interval.minTime == interval.minTime and eng_interval.maxTime == interval.maxTime:
+                    eng = eng_interval.mark.strip()
+        
+        if re.match(sw_name_pattern, tier.name):
+            for sw_interval in tier.intervals:
+                if sw_interval.minTime == interval.minTime and sw_interval.maxTime == interval.maxTime:
+                    sw = sw_interval.mark.strip()    
+    return eng, sw
+
+def process(file_name, folder_path):
+    tg = textgrid.TextGrid.fromFile(os.path.join(folder_path, file_name + '.TextGrid')) 
+    tier_name_pattern = transcription_tiers_map[file_name[:3]]
+
+    audio, sr = load_audio_for_file(folder_path, file_name)
+
+    rel_folder = os.path.relpath(folder_path, DIR_PATH).replace(os.sep, "_")
+
+    if audio is None:
+        return 0
+
+    for tier in tg.tiers:
+        if not re.match(tier_name_pattern, tier.name):
+            continue
+
+        non_empty_intervals = [interval for interval in tier.intervals if interval.mark.strip()]
+        if not non_empty_intervals:
+            print('THERE ARENT ANY NON EMPTY INTERVALS :(', folder_path, file_name)
+            return 0
+        
+        for interval in non_empty_intervals:
+            text_raw = interval.mark.strip()
+            try:
+                asmjeeg_str = annotations_processing.process_text(text_raw)
+            except AttributeError:
+                asmjeeg_str = text_raw
+            
+
+            start, end = interval.minTime, interval.maxTime
+            text_eng, text_sw = find_translations(tg.tiers, interval, file_name[:3])
+            
+            start_sample = int(start * sr)
+            end_sample = int(end * sr)
+            audio_segment = audio[start_sample:end_sample]
+
+            if len(audio_segment) == 0:
+                print("EMPTY AUDIO SEGMENT:", file_name, start, end)
+                continue
+
+            segment_id = f"{rel_folder}_{file_name}_{int(start * 1000):07d}_{int(end * 1000):07d}"
+            save_segment(OUT_PATH, segment_id, audio_segment, sr, asmjeeg_str, text_eng, text_sw)
+        
+        return 1 
+    return 0
+    
+
+def main():
+    os.makedirs(OUT_PATH, exist_ok=True)
+
+    directories = [d for d in os.listdir(DIR_PATH) if os.path.isdir(os.path.join(DIR_PATH, d))]
+
+    for dir in directories[18:19]:
+            inner_path = os.path.join(DIR_PATH, dir)
+            files = {file.split('.')[0] for file in os.listdir(inner_path) if file.endswith('.TextGrid')}
+            
+            for file in files:
+                process(file, inner_path)
+
+
+if __name__ == "__main__":
+    main()
